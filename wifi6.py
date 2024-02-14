0e03ae79c54965884a66d5be5aa99c8155effa53
KingHash23 <markcalvin2003@gmail.com>
2024-02-14 08:47:08 +0300

    wifi

--- wifi6.py
+++ wifi6.py
@@ -1,33 +1,34 @@
-import subprocess
-import re
-
-def get_nearby_wifi_networks():
-    try:
-        result = subprocess.check_output(["netsh", "wlan", "show", "networks"]).decode()
-        networks = re.findall(r"(?<=SSID\s*:\s*)([\w\s]+)(?=\r\n)", result)
-        return networks
-    except Exception as e:
-        print(f"Error: {e}")
-        return []
-
-def get_wifi_info(network):
-    try:
-        result = subprocess.check_output(["netsh", "wlan", "show", "profile", network, "key=clear"]).decode()
-        ssid = [line.split(":")[-1].strip() for line in result.split("\n") if "SSID name" in line][0]
-        security_type = [line.split(":")[-1].strip() for line in result.split("\n") if "Security key" in line][0]
-        password = [line.split(":")[-1].strip() for line in result.split("\n") if "Key Content" in line][0]
-        return ssid, security_type, password
-    except Exception as e:
-        print(f"Error: {e}")
-        return "", "", ""
-
-nearby_wifi_networks = get_nearby_wifi_networks()
-
-if nearby_wifi_networks:
-    for network in nearby_wifi_networks:
-        ssid, security_type, password = get_wifi_info(network)
-        if ssid:
-            print(f"SSID: {ssid}, Password: {password}")
-            break
-else:
-    print("No nearby Wi-Fi networks found.")
+def get_wifi_interface():
+    wifi = BDF222()
+    if len(wifi.interfaces()) <= 0:
+        print u'Wireless card interface not found!'
+        exit()
+    if len(wifi.interfaces()) == 1:
+        print u'Wireless card interface: %s'%(wifi.interfaces()[0].name())
+        return wifi.interfaces()[0]
+    else:
+        print '%-4s   %s'%(u'Serial number',u'Network card interface name')
+        for i,w in enumerate(wifi.interfaces()):
+            print '%-4s   %s'%(i,w.name())
+        while True:
+            iface_no = raw_input('Please select network card interface serial number:'.decode('utf-8').encode('gbk'))
+            no = int(iface_no)
+            if no>=0 and no < len(wifi.interfaces()):
+                return wifi.interfaces()[no]
+ def get_wifi_interface():
+    wifi = PyWiFi()
+    if len(wifi.interfaces()) &lt;= 0:
+        print u'Wireless card interface not found!'
+        exit()
+    if len(wifi.interfaces()) == 1:
+        print u'Wireless card interface: %s'%(wifi.interfaces()[0].name())
+        return wifi.interfaces()[0]
+    else:
+        print '%-4s   %s'%(u'Serial number',u'Network card interface name')
+        for i,w in enumerate(wifi.interfaces()):
+            print '%-4s   %s'%(i,w.name())
+        while True:
+            iface_no = raw_input('Please select network card interface serial number:'.decode('utf-8').encode('gbk'))
+            no = int(iface_no)
+            if no&gt;=0 and no &lt; len(wifi.interfaces()):
+                return wifi.interfaces()[no]
